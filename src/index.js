f6f0fa8c97d2e1457c9c24c9903ee22322fa13de
rezaMH0123 <mehnatkesh.r@gmail.com>
2023-01-06 04:28:04 -0800

    add app-promotion and two swiper

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import 'swiper/css/bundle'
-import './swiper.css'
+import "swiper/css/bundle";
+import "./swiper.css";
 
-import App from './App'
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
