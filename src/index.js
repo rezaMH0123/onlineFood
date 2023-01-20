c58ea0c8e490e16ee1c0fd6245964177bf87781b
rezaMH0123 <mehnatkesh.r@gmail.com>
2023-01-21 00:06:59 +0330

    change code for pwa

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
 import "swiper/css/bundle";
 import "./swiper.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import App from "./App";
 
@@ -14,3 +14,7 @@ root.render(
     <App />
   </BrowserRouter>
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
